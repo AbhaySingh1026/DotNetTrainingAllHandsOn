cc98aaf32be590dcf8bb7ca880d3724ea21b7168
Abhay Pratap Singh <63088563+nxtElonMusk@users.noreply.github.com>
2022-05-17 17:18:24 +0530

    Add files via upload

---
+++ Learning Class and Object/Animals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_Class_and_Object
+{
+    class Cat
+    {
+        public void ofCat()
+        {
+            Console.WriteLine("Meow Meow");
+            Console.ReadLine();
+        }
+    }
+    class Dog
+    {
+        public void ofDog()
+        {
+            Console.WriteLine("Bhaw Bhaw");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Learning Class and Object/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_Class_and_Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Animal;
+            Console.WriteLine("Please Enter Your Animal - ");
+            A:
+            Animal = Console.ReadLine();
+            if (Animal == "Cat")
+            {
+                Cat Voice = new Cat();
+                Voice.ofCat();
+            }
+            else if (Animal == "Dog")
+            {
+                Dog Voice = new Dog();
+                Voice.ofDog();
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Either Dog or Cat");
+                goto A;
+                Console.ReadLine();
+            }
+        }
+    }
+}
