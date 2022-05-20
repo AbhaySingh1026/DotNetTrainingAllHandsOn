b62b093b2b233a49152fb20882fd3e94d2a2ffd6
Abhay Pratap Singh <63088563+nxtElonMusk@users.noreply.github.com>
2022-05-20 11:26:08 +0530

    Add files via upload

---
+++ FILE HANDLING FOR READING/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FILE_HANDLING_FOR_READING
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Read obj = new Read();
+            obj.Readfrom();
+        }
+    }
+}
---
+++ FILE HANDLING FOR READING/Read.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FILE_HANDLING_FOR_READING
+{
+    public class Read
+    {
+        public void Readfrom()
+        {
+            FileStream fileStreamobj = new FileStream("E:\\KelltonTech\\.NET training kellton\\Real Training Started\\FILE HANDLING FOR READING\\ReadFrom.txt",FileMode.Open,FileAccess.Read);
+            StreamReader streamReaderobj = new StreamReader(fileStreamobj);
+            Console.WriteLine("Id\tSource\tDestination\tDate\tTime\tStatus\tNetwork");
+            while (streamReaderobj.Peek() > 0)
+            {
+                string line = streamReaderobj.ReadLine();
+                if(line != "")
+                {
+                    if (line.StartsWith("Date"))
+                    {
+                        string[] newLine1 = line.Split(' ');
+                        string[] newLine2 = newLine1[0].Split(':');
+                        Console.Write(newLine2[1] + "\t" + newLine1[1] + "\t");
+                        continue;
+                    }
+                    string[] newLine = line.Split(':');
+                    Console.Write(newLine[1]+"\t");
+                }
+                else{
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
