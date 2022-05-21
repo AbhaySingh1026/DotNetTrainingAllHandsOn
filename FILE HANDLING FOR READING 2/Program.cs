ca25e431cdc9cb38b9458609f324b9dfbd3d8dc6
Abhay Pratap Singh <63088563+nxtElonMusk@users.noreply.github.com>
2022-05-21 20:01:02 +0530

    Add files via upload

---
+++ FILE HANDLING FOR READING 2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Task
+//u have to create 4 functions
+//Succes(), Failed(), Missed(), Dailed()
+//wen u call the function name
+//you should display only part of rows
+//example if i call succes method. thn display success calls only
+
+namespace FILE_HANDLING_FOR_READING_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            A:
+            Console.WriteLine("For which status you want to see records - ");
+            Console.WriteLine("1. Success");
+            Console.WriteLine("2. Failed");
+            Console.WriteLine("3. Missed");
+            Console.WriteLine("4. Dialled");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    Read obj = new Read();
+                    obj.Reading("Success");
+                    break;
+                case 2:
+                    Read obj1 = new Read();
+                    obj1.Reading("Failed");
+                    break;
+                case 3:
+                    Read obj2 = new Read();
+                    obj2.Reading("Missed");
+                    break;
+                case 4:
+                    Read obj3 = new Read();
+                    obj3.Reading("Dialled");
+                    break;
+                default:
+                    Console.WriteLine("--------Oops u entered wrong number-------");
+                    Console.WriteLine("Please try again.");
+                    goto A;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Do you want to check another status also - type yes/no");
+            string ans1 = Console.ReadLine();
+            if ((ans1 == "yes") || (ans1 == "Yes")||(ans1=="YES"))
+            {
+                goto A;
+            }
+            return;
+
+        }
+    }
+}
---
+++ FILE HANDLING FOR READING 2/Read.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FILE_HANDLING_FOR_READING_2
+{
+    public class Read
+    {
+        public void Reading(string input)
+        {
+            string[] arr1 = new string[100];
+            string[] arr2 = new string[100];
+            string[] arr3 = new string[100];
+            FileStream fileStreamobj = new FileStream("E:\\KelltonTech\\.NET training kellton\\Real Training Started\\FILE HANDLING FOR READING 2\\ReadFrom.txt", FileMode.Open, FileAccess.Read);
+            StreamReader streamReaderobj = new StreamReader(fileStreamobj);
+            Console.WriteLine("Id\tSource\tDestination\tDate\tTime\tStatus\tNetwork");
+            string line1 = streamReaderobj.ReadLine();
+            while (streamReaderobj.Peek() > 0)
+            {
+                string line2 = streamReaderobj.ReadLine();
+                if (line2 != "")
+                {
+                    line1 = line1 +"\t"+ line2;
+                }
+                else if(line2 == "")
+                {
+                    if (line1.Contains(input))
+                    {
+                        arr1 = line1.Split('\t');
+                        for(int i = 0; i < arr1.Length; i++)
+                        {
+                            if (i == 3)
+                            {
+                                arr2 = arr1[i].Split(' ');
+                            }
+                            else
+                            {
+                                arr2 = arr1[i].Split(':');
+                            }
+                            Console.Write(arr2[1]+'\t');
+                        }
+                        Console.WriteLine();
+                        
+                    }
+                    line1 = streamReaderobj.ReadLine();
+                }
+
+            }
+            return;
+        }
+        
+    }
+}
