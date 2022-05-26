83e3fbaf6a69542404d169bf48435ab4e65baad3
nxtElonMusk <gourav7398@gmail.com>
2022-05-26 16:01:19 +0530

    Added Class Library Hands On

---
+++ CLASS LIBRARY HANDS ON/CALL LIBRARY CLASS/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CLASS_LIBRARY_HANDS_ON;
+
+// -------------------------------------I have stored information of 10 customers from ID 101 to 110----------------------------------------- //
+
+namespace CALL_LIBRARY_CLASS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerData customerDataobj = new CustomerData();
+            customerDataobj.InputDataInList();
+            TOP:
+            Console.WriteLine("1. ShowCustomerDetails");
+            Console.WriteLine("2. ShowEBill");
+            Console.WriteLine("3. ShowEBillReport For all customers");
+            int input = Convert.ToInt32(Console.ReadLine());
+            switch (input) {
+                case 1:
+                    Console.Write("Enter Customer Id - ");
+                    customerDataobj.ShowCustomerDetails(Convert.ToInt32(Console.ReadLine()));
+                    break;
+                case 2:
+                    Console.WriteLine("Enter Customer Id - ");
+                    customerDataobj.ShowEBill(Convert.ToInt32(Console.ReadLine()));
+                    break;
+                case 3:
+                    customerDataobj.ShowEBillReport();
+                    break;
+                default:
+                    Console.WriteLine("Please choose correct option");
+                    goto TOP;
+            }
+        }
+    }
+}
---
+++ CLASS LIBRARY HANDS ON/CustomerData.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASS_LIBRARY_HANDS_ON
+{
+    public class CustomerData
+    {
+        int customerId { get; set; }
+        string customerName { get; set; }
+        string city { get; set; }
+        long mobileNo { get; set; }
+        int age { get; set; }
+        int units { get; set; }
+        int perUnitCost { get; set; } = 10;
+        int totalCost { get; set; }
+        string paidOrNot { get; set; }
+
+        List<CustomerData> myList = new List<CustomerData>();
+        public void InputDataInList()
+        {
+            myList.Add(new CustomerData
+            {
+                customerId = 101,
+                customerName = "Abhay Singh",
+                city = "Sitapur",
+                mobileNo = 4654664646,
+                age = 21,
+                units = 100,
+                totalCost = units*perUnitCost,
+                paidOrNot = "Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 102,
+                customerName = "Vikas Singh",
+                city = "Sitapur",
+                mobileNo = 4654664646,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Not Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 103,
+                customerName = "Saurabh Singh",
+                city = "Sitapur",
+                mobileNo = 4654664646,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 104,
+                customerName = "Vibhu Singh",
+                city = "Sitapur",
+                mobileNo = 4654664646,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Not Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 105,
+                customerName = "Anubhav Singh",
+                city = "Sitapur",
+                mobileNo = 4654664646,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 106,
+                customerName = "Abhinav Singh",
+                city = "Sitapur",
+                mobileNo = 4654664646,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Not Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 107,
+                customerName = "Panda Singh",
+                city = "Sitapur",
+                mobileNo = 4654664646,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 108,
+                customerName = "Atul Singh",
+                city = "Sitapur",
+                mobileNo = 4654664646,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Not Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 109,
+                customerName = "Ankit Singh",
+                city = "Sitapur",
+                mobileNo = 4654664646,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Paid"
+            });
+            myList.Add(new CustomerData
+            {
+                customerId = 110,
+                customerName = "Vipin Singh",
+                city = "Sitapur",
+                mobileNo = 4654664646,
+                age = 21,
+                units = 100,
+                totalCost = units * perUnitCost,
+                paidOrNot = "Not Paid"
+            });
+        }
+        public void ShowCustomerDetails(int iD)
+        {
+            for(int i=0;i<myList.Count; i++)
+            {
+                if(iD == myList[i].customerId)
+                {
+                    Console.WriteLine(myList[i].customerId + " " + myList[i].customerName + " " + myList[i].city + " " + myList[i].mobileNo + " "
+                        + myList[i].age);
+                    break;
+                }
+            }
+            return;
+        }
+        public void ShowEBill(int iD)
+        {
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (iD == myList[i].customerId)
+                {
+                    Console.WriteLine(myList[i].units + " " + myList[i].totalCost);
+                    break;
+                }
+            }
+            return;
+        }
+        public void ShowEBillReport()
+        {
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i].customerId + " " + myList[i].customerName + " " + myList[i].city + " " + myList[i].mobileNo + " "
+                        + myList[i].age + myList[i].units + " " + myList[i].totalCost + " " + myList[i].paidOrNot);
+            }
+            return;
+        }
+    }
+}
