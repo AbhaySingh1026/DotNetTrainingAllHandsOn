033a502a80670d6e7eb59057274ba3c74548b261
nxtElonMusk <gourav7398@gmail.com>
2022-06-08 18:23:35 +0530

    Added 1 handOn of entering data in DB by console with dll file

---
+++ INSERT DATA IN DB BY CONSOLE/Data.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace INSERT_DATA_IN_DB_BY_CONSOLE
+{
+    public class Data
+    {
+        string connectiion = "Data Source=DESKTOP-I3CUF5I;Initial Catalog=BankDb;Integrated Security=True";
+        public string InsertData(int bookId, string bookName, string author, int price)
+        {
+            SqlConnection sqlConnection = new SqlConnection(connectiion);
+            SqlCommand sqlCommand = new SqlCommand("insert into BOOKDETAIL values("+bookId+", '"+bookName+"', '"+author+"', "+price+")", sqlConnection);
+            sqlConnection.Open();
+            sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            return "Data Inserted";
+        }
+    }
+}
---
+++ INSERT DATA IN DB BY CONSOLE/Run Data Dll File/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INSERT_DATA_IN_DB_BY_CONSOLE;
+
+namespace Run_Data_Dll_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bookId;
+            string bookName;
+            string author;
+            int price;
+            Data dataObj = new Data();
+            Console.Write("Enter book id - ");
+            bookId = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter book name - ");
+            bookName = Console.ReadLine();
+            Console.Write("Enter author name - ");
+            author = Console.ReadLine();
+            Console.Write("Enter price - ");
+            price = Convert.ToInt32(Console.ReadLine());
+            dataObj.InsertData(bookId, bookName,author,price);
+        }
+    }
+}
